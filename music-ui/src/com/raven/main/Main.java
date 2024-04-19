c1890f225d8a9aa35a6b89fbcc7f85d177b0ab31
EmreTaskend <etaskend21@ku.edu.tr>
2024-04-19 19:56:38 +0300

    asd

--- music-ui/src/com/raven/main/Main.java
+++ music-ui/src/com/raven/main/Main.java
@@ -180,38 +180,12 @@ public class Main extends javax.swing.JFrame {
     }//GEN-LAST:event_panelMovingMouseDragged
 
     public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new Main().setVisible(true);
             }
         });
     }
-
-    // Variables declaration - do not modify//GEN-BEGIN:variables
     private com.raven.component.Bottom bottom1;
     private com.raven.form.Form_Artists form_Artists1;
     private javax.swing.JLabel jLabel1;
@@ -222,5 +196,4 @@ public class Main extends javax.swing.JFrame {
     private com.raven.swing.Panel panel1;
     private javax.swing.JPanel panelMoving;
     private javax.swing.JScrollPane sp;
-    // End of variables declaration//GEN-END:variables
 }
